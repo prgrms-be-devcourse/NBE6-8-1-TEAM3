e61709dce38ce84516e23e7e4d5c7587fa8adc32
namgigun <namgigun@gmail.com>
2025-07-15 15:15:54 +0900

    feat : 상품, 장바구니 페이지
    페이지 틀 완성

--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -1,3 +1,31 @@
 export default function Page() {
-    return <div>상품 및 주문 페이지</div>   
+    return (
+        <>
+            <h1>Grids & Circles</h1>
+
+            <h2>상품목록</h2>
+
+            {/* 이후 DB로 받을 예정 */}
+            <div>상품1</div>
+            <div>상품2</div>
+            <div>상품3</div>
+
+
+            <h2>장바구니</h2>
+            
+
+            <div>이메일</div>
+            <input type="text" className="border-2 border-black"/>
+
+            <div>주소</div>
+            <input type="text" className="border-2 border-black"/>
+
+            <div>우편주소</div>
+            <input type="text" className="border-2 border-black"/>
+
+            <div>총금액</div>
+            
+            <button>주문하기</button>
+        </>
+    )
 }
