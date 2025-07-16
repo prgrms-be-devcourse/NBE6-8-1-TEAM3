6c57a143b4778b666acae770d9ded63490cc492b
ohsoohyuk <okzzangsh@gmail.com>
2025-07-16 14:23:42 +0900

    [BE] 상품 설명 변경

--- backend/src/main/java/com/backend/api/v1/products/init/BaseInitData.java
+++ backend/src/main/java/com/backend/api/v1/products/init/BaseInitData.java
@@ -29,9 +29,9 @@ public class BaseInitData {
     public void work1() {
         if (productsRepository.count() > 0) return;
 
-        productsRepository.save(new Products("아이스아메리카노", "아아", 4500, "images/아이스아메리카노"));
-        productsRepository.save(new Products("카페라떼", "카페라떼", 5000, "images/카페라떼"));
-        productsRepository.save(new Products("녹차라떼", "녹차라떼", 5300, "images/녹차라떼"));
-        productsRepository.save(new Products("치즈케이크", "치즈케이크", 6000, "images/치즈케이크"));
+        productsRepository.save(new Products("아이스아메리카노", "시원한 아메리카노로 여름을 날려버릴 수 있어요", 4500, "images/아이스아메리카노"));
+        productsRepository.save(new Products("카페라떼", "나 때는 카페라떼", 5000, "images/카페라떼"));
+        productsRepository.save(new Products("녹차라떼", "카페라떼가 싫으면 녹차라떼 GO", 5300, "images/녹차라떼"));
+        productsRepository.save(new Products("치즈케이크", "아아와 같이 먹으면 환상의 조합!", 6000, "images/치즈케이크"));
     }
 }
