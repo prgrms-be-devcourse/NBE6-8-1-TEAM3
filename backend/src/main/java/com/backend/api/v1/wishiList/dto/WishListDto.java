f6888cd66a7977d219ac75868fdb801cd969003f
KSH0326 <ths0074@naver.com>
2025-07-18 13:01:43 +0900

    feat:응답DTO 수정

--- backend/src/main/java/com/backend/api/v1/wishiList/dto/WishListDto.java
+++ backend/src/main/java/com/backend/api/v1/wishiList/dto/WishListDto.java
@@ -2,20 +2,27 @@ package com.backend.api.v1.wishiList.dto;
 
 import com.backend.api.v1.wishiList.entity.WishList;
 
+import java.util.List;
+
 public record WishListDto(
-        int id,
+
         String email,
         String address,
+        String zipCode,
         int totalCount,
-        int totalPrice
+        int totalPrice,
+        List<WishListItemDto> wishListItems
 ) {
     public WishListDto(WishList wishlist){
         this(
-                wishlist.getWishId(),
                 wishlist.getEmail(),
                 wishlist.getAddress(),
+                wishlist.getZipCode(),
                 wishlist.getTotalCount(),
                 wishlist.getTotalPrice()
+                , wishlist.getWishListItem().stream()
+                        .map(WishListItemDto::new)
+                        .toList()
         );
     }
 
